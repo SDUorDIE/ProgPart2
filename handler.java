4b3a7f28e1a04bcf3d9ee77faf4e0ce40298014c
SDUorDIE <164015427+SDUorDIE@users.noreply.github.com>
2025-06-26 23:27:59 +0200

    Update handler.java

--- handler.java
+++ handler.java
@@ -1,58 +1,108 @@
-
 package com.mycompany.progpart2;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.io.FileWriter;
+import java.io.FileReader;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
-public class handler {
+/**
+ * This class handles sending and storing messages.
+ * JSON functionality uses json-simple:
+ * 
+ * Attribution:
+ * https://code.google.com/archive/p/json-simple/
+ * 
+ * Maven Dependency:
+ * <dependency>
+ *   <groupId>com.googlecode.json-simple</groupId>
+ *   <artifactId>json-simple</artifactId>
+ *   <version>1.1.1</version>
+ * </dependency>
+ */
+public class Handler {
     private List<Message> sentMessages = new ArrayList<>();
     private List<Message> storedMessages = new ArrayList<>();
-    
-    public void proccessMessages(int total){
-        for(int i = 1; i<= total; i++){
+
+    public void processMessages(int total) {
+        for (int i = 1; i <= total; i++) {
             String recipient = javax.swing.JOptionPane.showInputDialog("Enter number:");
-            String Text = javax.swing.JOptionPane.showInputDialog("enter message(250 max characatrers:");
-            
-            if (Text.length()>250){
-                javax.swing.JOptionPane.showInputDialog(null, "message exceeds 250 characters by "+ (Text.length() - 250));
-                i--; continue;
+            String text = javax.swing.JOptionPane.showInputDialog("Enter message (250 max characters):");
+
+            if (text.length() > 250) {
+                javax.swing.JOptionPane.showMessageDialog(null, "Message exceeds 250 characters by " + (text.length() - 250));
+                i--;
+                continue;
             }
-            Message msg = new Message(i, recipient, Text);
-            if (!msg.checkRecipientCell()){
-                javax.swing.JOptionPane.showMessageDialog(null, "invalid phone number format.");
-                i--; continue;
+
+            Message msg = new Message(i, recipient, text);
+
+            if (!msg.checkRecipientCell()) {
+                javax.swing.JOptionPane.showMessageDialog(null, "Invalid phone number format.");
+                i--;
+                continue;
             }
+
             String action = msg.getSendOption();
-            
-            if (action.equals("send")){
-                sentMessages.add(msg);
-                javax.swing.JOptionPane.showMessageDialog(null, msg.toDisplayString()+ "\nMessage successfully sent");
-            }else if (action.equals("store")){
-                storedMessages.add(msg);
-                javax.swing.JOptionPane.showMessageDialog(null, "message stored");
-            }else{
-                javax.swing.JOptionPane.showMessageDialog(null, "message disregarded");
+
+            switch (action) {
+                case "send":
+                    sentMessages.add(msg);
+                    javax.swing.JOptionPane.showMessageDialog(null, msg.toDisplayString() + "\nMessage successfully sent.");
+                    break;
+                case "store":
+                    storedMessages.add(msg);
+                    javax.swing.JOptionPane.showMessageDialog(null, "Message stored.");
+                    break;
+                default:
+                    javax.swing.JOptionPane.showMessageDialog(null, "Message disregarded.");
             }
         }
-        java.swing.JOptionPane.showMessageDialog(null, "total messages sent:"+ sentMessages.size());
+
+        javax.swing.JOptionPane.showMessageDialog(null, "Total messages sent: " + sentMessages.size());
     }
-    public void storeMessagesToJson(){
+
+    public void storeMessagesToJson() {
         JSONArray jsonMessages = new JSONArray();
         for (Message msg : storedMessages) {
             JSONObject obj = new JSONObject();
             obj.put("messageID", msg.getMessageID());
-            obj.put("Hash",msg.getHash());
-            obj.put("Recipient",msg.getRecipient());
-            obj.put("Text",msg.getText());
+            obj.put("Hash", msg.getHash());
+            obj.put("Recipient", msg.getRecipient());
+            obj.put("Text", msg.getText());
             jsonMessages.add(obj);
         }
-        try (FileWriter file = new FileWriter("storedMessages.json")){
+        try (FileWriter file = new FileWriter("storedMessages.json")) {
             file.write(jsonMessages.toJSONString());
             file.flush();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public void readMessagesFromJson() {
+        try (FileReader reader = new FileReader("storedMessages.json")) {
+            JSONParser parser = new JSONParser();
+            JSONArray jsonMessages = (JSONArray) parser.parse(reader);
+
+            storedMessages.clear(); 
+
+            for (Object obj : jsonMessages) {
+                JSONObject jsonObj = (JSONObject) obj;
+                String id = (String) jsonObj.get("messageID");
+                String hash = (String) jsonObj.get("Hash");
+                String recipient = (String) jsonObj.get("Recipient");
+                String text = (String) jsonObj.get("Text");
+
+                Message msg = new Message(Integer.parseInt(id), recipient, text);
+                storedMessages.add(msg);
+            }
+
+            javax.swing.JOptionPane.showMessageDialog(null, "Stored messages loaded from JSON.");
+        } catch (Exception e) {
+            javax.swing.JOptionPane.showMessageDialog(null, "Error reading JSON: " + e.getMessage());
         }
     }
 }
